e9ece2d1d16d8f007ac2f2b085e6feaefd50e497
James Arasim <jaemzware@hotmail.com>
2014-12-18 18:20:41 -0800

    took out screenshot

--- src/test/java/com/jaemzware/board/BoardScrub.java
+++ src/test/java/com/jaemzware/board/BoardScrub.java
@@ -608,7 +608,7 @@ public class BoardScrub extends CodeBase {
             }
             else{
                 System.out.println("WARNING: TITLETEXT AT XPATH:"+titleTextXpath+" WAS NOT FOUND AFTER:"+quickWaitMilliSeconds+"ms");
-                ScreenShot();
+                
             }
 
 
