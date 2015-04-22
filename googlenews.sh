64c695c6984837e85e07b30bcf3c23c186a117c9
James Arasim <jaemzware@hotmail.com>
2015-04-22 08:38:10 -0700

    upped wait time

--- googlenews.sh
+++ googlenews.sh
@@ -1,1 +1,1 @@
-mvn -Dtest=BoardScrub#BuildPageOfFoundLinks -Dbrowser=CHROME -Denvironment=googlenews -Dinput="https://news.google.com/" -Dnogrid -DaNumber=0 -DwaitAfterPageLoadMilliSeconds=5000 test
+mvn -Dtest=BoardScrub#BuildPageOfFoundLinks -Dbrowser=CHROME -Denvironment=googlenews -Dinput="https://news.google.com/" -Dnogrid -DaNumber=0 -DwaitAfterPageLoadMilliSeconds=10000 test
