9fd0492dc5f29b5ed1d8cb8b1de5f8a7e41e996d
JAMES ARASIM <jaemzware@hotmail.com>
2015-09-09 12:09:38 -0700

    new script for tnaboard

---
+++ tnaboaard.sh
@@ -0,0 +1,1 @@
+mvn -Dtest=BoardScrub#BuildPageOfFoundLinks -Dbrowser=FIREFOX -Denvironment=tnaboard -Dinput="https://www.tnaboard.com/forumdisplay.php?116-OR-Provider-Ads&s=&pp=30&prefixid=portland&field_dvb_service_type=Escort" -DaNumber=200 -Dnogrid -Dreport=pdxtna -DwaitAfterPageLoadMilliSeconds=0 test
