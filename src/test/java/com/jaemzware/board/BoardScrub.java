8d1b322470be32647ad328111b7cbd02902627dc
JAMES ARASIM <jaemzware@hotmail.com>
2016-03-14 21:55:27 -0700

    made report name more predictable (took out datestamp) so it can be made the body of an email

--- src/test/java/com/jaemzware/board/BoardScrub.java
+++ src/test/java/com/jaemzware/board/BoardScrub.java
@@ -361,7 +361,7 @@ public class BoardScrub extends CodeBase {
             report = "NONAMEREPORT";
         }
         
-        String fileName = "index"+getDateStamp()+"-" + report + ".htm";
+        String fileName = "index" + report + ".htm";
         try (PrintWriter writer = new PrintWriter(fileName, "UTF-8")) {
             writer.println(HtmlReportHeader("BoardScrub:<a href='"+input+"' target='_blank'>"+input+"</a>"));
             
